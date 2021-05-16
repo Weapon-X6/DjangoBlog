b8cedb74a83e2298f220dbe7dce6ba287214dd47
waffe_x20 <argueta_eduardo@yahoo.de>
2021-05-15 22:12:25 -0600

    add Post views

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from rest_framework import generics
 from blog import serializers
 from django.contrib.auth.models import User
+from blog.models import Post
 
 
 class UserList(generics.ListAPIView):
@@ -14,3 +15,19 @@ class UserList(generics.ListAPIView):
 class UserDetail(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = serializers.UserSerializer
+
+
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = serializers.PostSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = serializers.PostSerializer
+
+
+
