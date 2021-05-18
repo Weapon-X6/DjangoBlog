b9912c4727153a305a567ce06ca2cffc3c369e4a
waffe_x20 <argueta_eduardo@yahoo.de>
2021-05-17 21:34:00 -0600

    add Comments

--- blog/serializers.py
+++ blog/serializers.py
@@ -1,19 +1,31 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
-from .models import Post  # or blog.models
+from .models import Post, Comment  # or blog.models
 
 
 class UserSerializer(serializers.ModelSerializer):
     posts = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+    comments = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'posts']
+        fields = ['id', 'username', 'posts', 'comments']
 
 
 class PostSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
+    comments = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
 
     class Meta:
         model = Post
-        fields = ['id', 'title', 'body', 'owner']
+        fields = ['id', 'title', 'body', 'owner', 'comments']
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    # eq. post = serializers.PrimaryKeyRelatedField(queryset=Post.objects.all())
+
+    class Meta:
+        model = Comment
+        fields = ['id', 'body', 'owner', 'post']
+
--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from rest_framework import generics
 from blog import serializers
 from django.contrib.auth.models import User
-from blog.models import Post
+from blog.models import Post, Comment
 from rest_framework import permissions
 from blog.permissions import IsOwnerOrReadOnly
 
@@ -32,6 +32,22 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = serializers.PostSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+
+class CommentList(generics.ListCreateAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = serializers.CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = serializers.CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
 
 
 
